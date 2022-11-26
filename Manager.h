db3fc3a664ef68759424e6d1f0fd6abf90ecff42
sankiong <semen.ryab0v@yandex.ru>
2022-11-26 17:42:22 +0300

    just check

--- Manager.h
+++ Manager.h
@@ -1,4 +1,9 @@
 #pragma once
+#include <vector>
 class Manager {
+private:
+    int _mood;
+    int _shift_time;
+    std::vector<int> _not_fav_id;
 };
 
--- Warehouse.h
+++ Warehouse.h
@@ -1,5 +1,8 @@
 #pragma once
-
+#include<iostream>
+#include<vector>
 class Warehouse {
+private:
+
 };
 
