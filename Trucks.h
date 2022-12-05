53c5d6a3906512f6a29365be64530b544096ffdb
Павел Чашников <pavelchash@list.ru>
2022-12-05 15:40:54 +0300

    Написал методы к Truck

--- Trucks.h
+++ Trucks.h
@@ -1,10 +1,19 @@
 #pragma once
 #include<iostream>
 #include<vector>
+//пока что вместо структуры time используется инт, думаю как лучше сделать наследование пока что
 class Trucks
 {
+public:
+    int getCapacity();
+    int getDeliveryTime();
+    std::vector<int>* getTime();
+    void setCapacity(int capacity);
+    void setDeliveryTime(int delivery_time);
+    void addTime(int time);
+    bool isBusy(int time);
 private:
     std::vector<int> _time;
-    int _capasity, _delivery_time;
+    int _capacity, _delivery_time;
 };
 
