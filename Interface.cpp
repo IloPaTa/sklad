efbbad539f47b12204bf80c36e8b3a0d692f077a
sankficeba <semen.ryab0v@yandex.ru>
2022-12-28 03:12:27 +0300

    cc

--- Interface.cpp
+++ Interface.cpp
@@ -38,8 +38,8 @@ Interface::Interface()
 
 
 
-    setObject(sf::Vector2f(400, 75), sf::Vector2f(1200, 0), "");
-    _current_date = 1;
+    setObject(sf::Vector2f(400, 75), sf::Vector2f(1100, 0), "");
+    _current_date = 0;
     _text_current_date = _texts[_texts.size() - 1];
 
     // заголовки на стартовой странице 
@@ -499,11 +499,12 @@ void Interface::input()
                         _event = "warehouse";
                     }
                     else if (i->getId() == "next day") {
+                    
                       //  _manager.processOrder(_whouse);
                         _whouse->updateItems();
                         createNextdayButtons();
                         _event = "next day";
-
+                        _current_date++;
                         _delta_y = 0;
                         map_lack_products = _manager.funcShortage(_whouse);
                         it = map_lack_products.begin();
@@ -537,7 +538,7 @@ void Interface::input()
                         std::vector<std::pair<Item*, int>> expired_products = _whouse->getDelayItem(7);
                         for (auto i : expired_products)
                         {
-                            //i->getName(); i->getShelfLife()
+                            //i->getName(); i->getShelfLife()а для 
                         }
 
                         _manager.getProductsFromWhOrder(_whouse);
